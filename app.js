f481784f28d1dcd4d02170703e631b5109c3db3a
YasminVargas <yasmiin.vargass@gmail.com>
2023-03-31 20:32:47 -0300

    initial commit

--- app.js
+++ app.js
@@ -1,47 +1,47 @@
 const button = document.querySelector(".addModal");
 const modal = document.querySelector(".modal");
 const modalContainer = document.querySelector(".modalContainer");
+const mainCard = document.querySelector("#mainCard");
 const form = document.querySelector("#modalForm");
 const modalButtonSend = document.querySelector(".modalButtonSend");
-const modalButtonCancel = document.querySelector(".modalButtonCancel");
+const modalButtonCancel = document.querySelector(".close");
+const closeB = document.querySelector(".closeButton");
 
-let openModal = () => {
-  modalContainer.style.backgroundColor = `rgba(${0}, ${0}, ${0}, ${0.5})`;
-  modalContainer.style.display = "flex";
-};
-
+let save = [];
 const nameInput = form.elements.name;
 const docName = form.elements.doctorName;
 const doctor = form.elements.doctor;
 const dateForm = form.elements.dateForm;
 const hourForm = form.elements.hourForm;
-const mainCard = document.querySelector("#mainCard");
-const closeB = document.querySelector(".closeButton");
 
-button.addEventListener("click", function (e) {
-  openModal();
-});
+function modalEvents() {
+  button.addEventListener("click", function (e) {
+    openModal();
+  });
 
-let save = [];
+  modalButtonSend.addEventListener("click", function (evt) {
+    addCard();
+    closeModal();
+  });
 
-function add() {
-  if (localStorage.meuArr) {
-    save = JSON.parse(localStorage.getItem("meuArr")) || [];
-  }
-  let newName = document.querySelector("#name").value;
-  let newDoctorName = document.querySelector("#doctorName").value;
-  let newDoctor = document.querySelector("#doctor").value;
-  let newDate = document.querySelector("#dateForm").value;
-  let newHour = document.querySelector("#hourForm").value;
-  save.push(newName);
-  save.push(newDoctorName);
-  save.push(newDoctor);
-  save.push(newDate);
-  save.push(newHour);
-  localStorage.myArr = JSON.stringify(save);
+  modalButtonCancel.addEventListener("click", (event) => {
+    console.log("test");
+    closeModal();
+  });
 }
 
-function card() {
+function openModal() {
+  modalContainer.style.backgroundColor = `rgba(${0}, ${0}, ${0}, ${0.5})`;
+  modalContainer.style.display = "flex";
+}
+
+function closeModal() {
+  modalContainer.style.backgroundColor = "none";
+  modalContainer.style.display = "none";
+  form.reset();
+}
+
+function addCard() {
   const div = document.createElement("div");
   div.className = "cardContainer";
   mainCard.append(div);
@@ -65,61 +65,66 @@ function card() {
   ul.append(date);
   ul.append(hour);
 
-  add();
+  updateLocalStorage();
 }
 
-modalButtonSend.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  card();
+function updateLocalStorage() {
+  let cards = getLocalStorage();
 
-  modalContainer.style.backgroundColor = "none";
-  modalContainer.style.display = "none";
-  nameInput.value = "";
-  docName.value = "";
-  doctor.value = "";
-  dateForm.value = "";
-  hourForm.value = "";
-});
+  cards.push({
+    name: nameInput.value,
+    docName: docName.value,
+    doctor: doctor.value,
+    dateForm: dateForm.value,
+    hourForm: hourForm.value,
+  });
 
-modalButtonCancel.addEventListener("click", (event) => {
-  event.preventDefault();
-  modalContainer.style.backgroundColor = "none";
-  modalContainer.style.display = "none";
-});
+  let json_cards = JSON.stringify(cards);
 
-closeB.addEventListener("click", (events) => {
-  events.preventDefault();
-  modalContainer.style.backgroundColor = "none";
-  modalContainer.style.display = "none";
-});
+  localStorage.setItem("cards", json_cards);
+}
 
-// function loadCard() {
-//   if (localStorage.myArr) {
-//     save = JSON.parse(localStorage.getItem("myArr"));
-//   }
-//   for (let info in save) {
-//     const div = document.createElement("div");
-//     div.className = "cardContainer";
-//     mainCard.append(div);
-//     const ul = document.createElement("ul");
-//     ul.className = "ulCard";
-//     div.append(ul);
-//     const name = document.createElement("li");
-//     const docNameCard = document.createElement("li");
-//     const doc = document.createElement("li");
-//     const date = document.createElement("li");
-//     const hour = document.createElement("li");
-//     name.innerText = save[i];
-//     docNameCard.innerText = save[i];
-//     doc.innerText = save[i];
-//     date.innerText = save[i];
-//     hour.innerText = save[i];
+function getLocalStorage() {
+  let json_cards = localStorage.getItem("cards");
 
-//     ul.append(name);
-//     ul.append(docNameCard);
-//     ul.append(doc);
-//     ul.append(hour);
-//     ul.append(date);
-//   }
-// }
-// loadCard();
+  if (json_cards) {
+    let cards = JSON.parse(json_cards);
+
+    return cards;
+  }
+
+  return [];
+}
+
+function loadCard() {
+  let cards = getLocalStorage();
+
+  for (let info of cards) {
+    const div = document.createElement("div");
+    div.className = "cardContainer";
+    mainCard.append(div);
+    const ul = document.createElement("ul");
+    ul.className = "ulCard";
+    div.append(ul);
+    const name = document.createElement("li");
+    const docNameCard = document.createElement("li");
+    const doc = document.createElement("li");
+    const date = document.createElement("li");
+    const hour = document.createElement("li");
+
+    name.innerText = info.name;
+    docNameCard.innerText = info.docName;
+    doc.innerText = info.doctor;
+    date.innerText = info.dateForm;
+    hour.innerText = info.hourForm;
+
+    ul.append(name);
+    ul.append(docNameCard);
+    ul.append(doc);
+    ul.append(hour);
+    ul.append(date);
+  }
+}
+
+loadCard();
+modalEvents();
